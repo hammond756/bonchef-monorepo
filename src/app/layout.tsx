7797540806c538c22b2c858c12f7448edd0c7454
Aron Hammond <aron.hammond@persgroep.net>
2025-03-30 17:50:08 +0200

    Revert layout changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NuqsAdapter>
           <Sidebar />
