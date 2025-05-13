102f74052e6a565deebb0d637f8cd8ca77cd601c
Aron Hammond <aron.hammond@persgroep.net>
2025-05-13 20:49:20 +0200

    Make public recipes api available

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,15 @@ const nextConfig: NextConfig = {
         ],
       },
       {
+        source: "/api/public/recipes",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+      {
         source: "/api/public/teaser",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
