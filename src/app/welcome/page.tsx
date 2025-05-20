5ee1971dd4b5f4aad44e2ba73932c7690894f4aa
Aron Hammond <aron.hammond@persgroep.net>
2025-05-20 22:22:32 +0200

    Improve copy welcome page

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -15,19 +15,18 @@ export default function WelcomePage() {
           height={120}
           className="mb-2"
         />
-        <h1 className="text-3xl font-bold text-center">Bonchef</h1>
-        <p className="text-lg text-center text-slate-600 mb-4">
+        <h1 className="text-3xl font-bold text-center">Welkom bij Bonchef</h1>
+        <p className="text-lg text-center text-slate-600 mb-4 font-bold">
           Jouw sociale kookboek
         </p>
         <ul className="text-left text-slate-700 mb-6 space-y-2">
-          <li>• Bewaar al jouw recepten op één plek</li>
-          <li>• Laat je inspireren door enthousiaste koks</li>
-          <li>• Deel jouw recepten met je vrienden</li>
+          <li>• Bewaar al je favoriete recepten op één plek</li>
+          <li>• Ontdek inspiratie van gepassioneerde koks</li>
+          <li>• Deel je eigen creaties met vrienden</li>
         </ul>
         <div className="flex flex-col items-center gap-2">
-          <span className="text-base text-slate-700">Wil je mee doen? Plaats dan je eerste recept</span>
           <Link href="/first-recipe">
-            <Button className="w-32">Begin</Button>
+            <Button>Voeg je eerste recept toe</Button>
           </Link>
         </div>
       </div>
