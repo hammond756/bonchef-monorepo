20a486a5aa8fb69f2db64bfba3988f1346da568b
Aron Hammond <aron.hammond@persgroep.net>
2025-09-02 19:22:03 +0200

    Starter example expo-share-intent

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
-import { Stack } from "expo-router";
+import { Slot, useRouter } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
+import { ShareIntentProvider } from "expo-share-intent";
+
+export default function Layout() {
+  const router = useRouter();
+
+  return (
+    <ShareIntentProvider
+      options={{
+        debug: true,
+        resetOnBackground: true,
+        onResetShareIntent: () =>
+          // used when app going in background and when the reset button is pressed
+          router.replace({
+            pathname: "/",
+          }),
+      }}
+    >
+      <Slot />
+    </ShareIntentProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,43 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { useShareIntentContext } from "expo-share-intent";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+export default function Home() {
+  const router = useRouter();
+
+  const { hasShareIntent } = useShareIntentContext();
+
+  useEffect(() => {
+    if (hasShareIntent) {
+      // we want to handle share intent event in a specific page
+      console.debug("[expo-router-index] redirect to ShareIntent screen");
+      router.replace({
+        pathname: "shareintent",
+      });
+    }
+  }, [hasShareIntent]);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Welcome to Expo Share Intent Demo !</Text>
+      <Text>Try to share a content to access specific page</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 10,
+  },
+});
