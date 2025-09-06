b625a536c82dd2f29b8c91695669604cabf45c13
Aron Hammond <aron.hammond@persgroep.net>
2025-09-06 20:18:56 +0200

    Add env vars to build task

--- apps/web/src/components/dishcovery/dishcovery-camera.tsx
+++ apps/web/src/components/dishcovery/dishcovery-camera.tsx
@@ -68,7 +68,7 @@ export function DishcoveryCamera({ onPhotoCaptured }: DishcoveryCameraProps) {
     }
 
     return (
-        <div className="flex h-full flex-col">
+        <div className="flex h-[100dvh] flex-col">
             {/* Header */}
             <div className="absolute top-0 right-0 left-0 z-40 flex items-center justify-between p-4">
                 <CloseButton onClick={() => router.back()} />
